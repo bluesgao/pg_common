7b6107ed9168cf7e7beca5d674701ce721cabc07
blues <>
2025-05-28 14:09:50 +0800

    Auto commit from script

--- pkg/errcode/game.go
+++ pkg/errcode/game.go
@@ -1,6 +1,8 @@
 package errcode
 
 var (
-	GameRequestErr = New(90001, "game request prams err")
-	GameNoFoundErr = New(90002, "game no found")
+	GameRequestErr     = New(90001, "game request prams err")
+	GameNoFoundErr     = New(90002, "game no found")
+	GameInfoNoFoundErr = New(90003, "game info no found")
+	GameCreateErr      = New(90004, "game create err")
 )
